e5b5faa1e686e673f815dde94931491dbc93f6b3
popote <yvan.bladet@gmail.com>
2020-12-03 08:15:23 +0100

    Integration Work In Progress
    scripte
    create:
    -ButtonComponent
    -DoorComponent
    -IntractComponent
    -SecretDoorComponent
    -IInteractebe

---
+++ Assets/Scripte/Component/InteractComponent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class InteractComponent : MonoBehaviour
+{
+    public GameObject Interaction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick(InputAction.CallbackContext ctx)
+    {
+        if (ctx.started)
+        {
+            Debug.Log("click");
+        }
+        Interaction.GetComponent<IInteracteble>().Intecract();
+    }
+}
