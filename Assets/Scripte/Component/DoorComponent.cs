33c11031b2ef51c3e2f670aa29498b6b346ae454
popote <yvan.bladet@gmail.com>
2020-12-04 04:32:42 +0100

    Build02

--- Assets/Scripte/Component/DoorComponent.cs
+++ Assets/Scripte/Component/DoorComponent.cs
@@ -1,9 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DoorComponent : MonoBehaviour , IInteracteble
 {
+    public Sprite Icone;
+    public GameObject UIImage;
+    public Vector3 NewPos;
+
+    private GameObject _panel;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -18,33 +25,39 @@ public class DoorComponent : MonoBehaviour , IInteracteble
 
     public void Intecract(InteractComponent.SelectStat selectStat)
     {
-        Debug.Log(" la porte est ouverte");
-
-        transform.position += transform.forward;
+       
     }
 
     public void SetPreselctable()
     {
-        throw new System.NotImplementedException();
+        _panel.transform.localScale = Vector3.one*2;
     }
 
     public void SetSelectable()
     {
-        throw new System.NotImplementedException();
+        Transform Canevas = GameObject.Find("Canvas").GetComponent<Canvas>().transform;
+        _panel = Instantiate(UIImage, Canevas);
+        _panel.AddComponent<FollowWorldUIComponent>();
+        _panel.GetComponent<FollowWorldUIComponent>().lookAt = transform;
+        _panel.GetComponent<Image>().sprite = Icone;
     }
 
     public void DePreselectable()
     {
-        throw new System.NotImplementedException();
+        _panel.transform.localScale = Vector3.one;
     }
 
     public void DesetSelectable()
     {
-        throw new System.NotImplementedException();
+        Destroy(_panel);
     }
 
     public bool USe(PlayerInfoComponent playerInfoComponent)
     {
-        throw new System.NotImplementedException();
+        transform.position = NewPos;
+        transform.Rotate(0,-90,0);
+        Destroy(_panel);
+        Destroy(this);
+        return true;
     }
 }
