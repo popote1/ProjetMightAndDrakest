9448584aec2cfa23083ef9916947ffccc5122fe1
popote <yvan.bladet@gmail.com>
2020-12-17 19:46:07 +0100

    MoreFeatures

---
+++ Assets/Scripte/ScriptableObject/SOSpecialEffectAddWeapon.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (fileName = "SOSpecialEffectAddWeapon", menuName = "SO/Combat/SOSpecialEffect/AddWeapon")]
+public class SOSpecialEffectAddWeapon : SOSpecialEffectGeneral
+{
+    public List<SOWeapon> PossibleWeapons;
+    
+    public override bool CheckForUse(FightComonent fightComonent, List<EnnemiCombatUIComponent> ennemiCombatUIComponents, int weaponIndex)
+    {
+        return true;
+    }
+    public override void MakeSpecialEffect(FightComonent fightComonent,List<EnnemiCombatUIComponent> ennemiCombatUIComponents, int weaponIndex)
+    {
+      // fightComonent.PlayerInfoComponent.
+    }
+}
